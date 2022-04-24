6975550217fd69e50d13b60c950dbb751c517f3a
Casey Fleser <cfleser@gmail.com>
2022-04-24 08:50:14 -0500

    Not totally ugly web page scaffolding

--- ServerMgr.cpp
+++ ServerMgr.cpp
@@ -30,27 +30,22 @@ R"==(<!doctype html><html lang='en'>
 <body style="width:300px">
   <h1>File Upload</h1>
   <div><a href="/">Home</a></div>
-  <hr>
-  <div id='zone' style='width:16em;height:12em;padding:10px;background-color:#ddd'>Drop files here...</div>
+  <hr><div id='zone' style='width:16em;height:12em;padding:10px;background-color:#ddd'>Drop files here...</div>
 
   <script>
-    // allow drag&drop of file objects 
     function dragHelper(e) {
-      e.stopPropagation();
-      e.preventDefault();
+        e.stopPropagation();
+        e.preventDefault();
     }
 
-    // allow drag&drop of file objects 
     function dropped(e) {
-      dragHelper(e);
-      var fls = e.dataTransfer.files;
-      var formData = new FormData();
-      for (var i = 0; i < fls.length; i++) {
-        formData.append('file', fls[i], '/' + fls[i].name);
-      }
-      fetch('/', { method: 'POST', body: formData }).then(function () {
-        window.alert('done.');
-      });
+        dragHelper(e);
+        var fls = e.dataTransfer.files;
+        var formData = new FormData();
+        for (var i = 0; i < fls.length; i++) {
+            formData.append('file', fls[i], '/' + fls[i].name);
+        }
+        fetch('/', { method: 'POST', body: formData }).then(function () { window.alert('done.'); });
     }
     var z = document.getElementById('zone');
     z.addEventListener('dragenter', dragHelper, false);
